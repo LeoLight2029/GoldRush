cd24ef867c24fca3fb45fd3e5815d1abc3cb6a01
LeoLight2029 <80296784+LeoLight2029@users.noreply.github.com>
2024-06-01 22:15:49 -0400

    Update transportText2.js

--- textScreens/transportText2.js
+++ textScreens/transportText2.js
@@ -44,4 +44,5 @@ function dialouge() {
   function goToNextPage() {
     window.location.href = 'transport/transport.html';
   }
+
   document.getElementById('info').textContent=`Hearts: ${localStorage.getItem('heart')} Cash: ${localStorage.getItem('cash')}`
